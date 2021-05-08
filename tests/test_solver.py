dce50055bb39a3c71cdba820573dcc32e0e83d9e
Kotaro Terada <kotarot@apache.org>
2021-05-08 23:07:55 +0900

    Add solver test

---
+++ tests/test_solver.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Kotaro Terada
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+import rectangle_packing_solver as rps
+from example_data import example_problem
+
+
+def test_solver(example_problem):
+    problem = rps.Problem(rectangles=example_problem)
+    solver = rps.Solver()
+    solution = solver.solve(problem)
+
+    assert isinstance(solution, rps.Solution)
+    assert isinstance(solution.sequence_pair, rps.SequencePair)
+    assert isinstance(solution.floorplan, rps.Floorplan)
+
+    # The optimal solution is any of ([0, 1, 3, 2], [3, 0, 2, 1]), ([1, 2, 0, 3], [2, 3, 1, 0]), ([2, 3, 1, 0], [1, 2, 0, 3]), or ([3, 0, 2, 1], [0, 1, 3, 2])
+    optimal_0 = solution.sequence_pair.pair == ([0, 1, 3, 2], [3, 0, 2, 1]) and solution.floorplan.boundary_box == (8, 7.2)
+    optimal_1 = solution.sequence_pair.pair == ([1, 2, 0, 3], [2, 3, 1, 0]) and solution.floorplan.boundary_box == (8, 7.2)
+    optimal_2 = solution.sequence_pair.pair == ([2, 3, 1, 0], [1, 2, 0, 3]) and solution.floorplan.boundary_box == (8, 7.2)
+    optimal_3 = solution.sequence_pair.pair == ([3, 0, 2, 1], [0, 1, 3, 2]) and solution.floorplan.boundary_box == (8, 7.2)
+    assert optimal_0 or optimal_1 or optimal_2 or optimal_3
+
+    assert solution.floorplan.area == 57.6

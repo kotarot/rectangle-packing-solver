db0eb421b515fe8f9dc6ef38a82f5338ddd416aa
Kotaro Terada <kotarot@apache.org>
2022-02-06 05:16:22 +0900

    Copyright year to 2022

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2021 Kotaro Terada
+# Copyright 2022 Kotaro Terada
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Kotaro Terada
+# Copyright 2022 Kotaro Terada
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
